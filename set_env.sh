1a10da94316523dcb873e0e249935f0d60914be4
Baffoe Abraham Acquah <99039877+BestpointTutorials@users.noreply.github.com>
2022-08-13 07:18:04 +0000

    initial commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udagramdev
+export POSTGRES_PASSWORD=bv2000gee4
+export POSTGRES_HOST=dev-instance.ct9k4gfnrdtq.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagramsbuckets
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
